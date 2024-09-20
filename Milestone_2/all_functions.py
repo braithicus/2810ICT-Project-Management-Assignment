b0c53fe879e76d03b452b5f8f48f968909f62053
William-JosephSimons <billyjoe.ds05@gmail.com>
2024-09-20 14:00:31 +1000

    added  g to mg conversion function

--- Milestone_2/all_functions.py
+++ Milestone_2/all_functions.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import re
+import matplotlib.pyplot as plt
 
 def load_data(filepath):
   try:
@@ -9,6 +10,24 @@ def load_data(filepath):
     # can be our file error number
     # if 69 do stuff etc 
     return 69
+  
+def to_mg(frame):
+  columns_in_g = [
+    'Fat', 
+    'Saturated Fats', 
+    'Monounsaturated Fats', 
+    'Polyunsaturated Fats', 
+    'Carbohydrates', 
+    'Sugars', 
+    'Protein', 
+    'Dietary Fiber', 
+    'Water'
+  ]
+
+  # MODIFIES ORIGINAL FRAME
+  # dataframe all in mg now
+  frame[columns_in_g] *= 1000
+
 
 def search_function(keyword, tableData):
     # returns boolean series of matches ignores casing
@@ -19,3 +38,11 @@ def search_function(keyword, tableData):
 
 def num_of_rows(num):
   return f"Number Of Results: {str(num)}"
+
+
+def nutrition_breakdown(foodRow, type='Bar'):
+  nutrients = []
+  if type == 'Bar':
+    nutrients
+  elif type == 'Pie':
+    nutrients
