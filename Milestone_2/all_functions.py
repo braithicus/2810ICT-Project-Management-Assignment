702a61fe26fb7848b421fa0f5c218ab44970bf93
William-JosephSimons <billyjoe.ds05@gmail.com>
2024-09-24 12:10:14 +1000

    added range and filter feature as well as comments

--- Milestone_2/all_functions.py
+++ Milestone_2/all_functions.py
@@ -29,20 +29,42 @@ def to_mg(frame):
   frame[columns_in_g] *= 1000
 
 
-def search_function(keyword, tableData):
+def search_function(keyword, dFrame):
     # returns boolean series of matches ignores casing
-    loc = [bool(re.search(keyword, name, re.IGNORECASE)) for name in tableData["food"]]
+    loc = [bool(re.search(keyword, name, re.IGNORECASE)) for name in dFrame["food"]]
     # returns new dataframe with only matches, and the number of results
-    return tableData[loc], sum(loc)
+    return dFrame[loc], sum(loc)
 
 
 def num_of_rows(num):
   return f"Number Of Results: {str(num)}"
 
 
+# TODO: Charts unreadable and hideous
+# USE: result.iloc[0, 1:] to get a foodRow series that retains column names 
+# (the above statement gets first foodRow from dataframe and skips the first column which is the food name)
 def nutrition_breakdown(foodRow, type='Bar'):
-  nutrients = []
   if type == 'Bar':
-    nutrients
+    plt.bar(foodRow.index, foodRow.values)
+    plt.show()
   elif type == 'Pie':
-    nutrients
+    plt.pie(foodRow.values, labels=foodRow.index, autopct='%1.1f%%', shadow=True)
+    plt.axis('equal')
+    plt.show()
+
+# we could pass this new dataframe back into the search feature with no keyword
+# to output to the search space. Once the search space is implemented ofc
+def nutrition_range_filter(dFrame, nutrientName, minVal, maxVal):
+  return dFrame[dFrame[nutrientName].between(minVal, maxVal)]
+
+# same idea as range feature
+def nutrition_level_feature(dFrame, nutrientName, level):
+  nutrientColumn= dFrame[nutrientName]
+  maxVal = nutrientColumn.max()
+  lowPercent, highPercent = maxVal * (33 / 100), maxVal * (66 / 100)
+  if level == 'Low':
+    return dFrame[nutrientColumn < lowPercent]
+  elif level == 'Mid':
+    return dFrame[nutrientColumn.between(lowPercent, highPercent)]
+  elif level == 'High':
+    return dFrame[nutrientColumn > highPercent]
