4db6e6a39e7b1bca5f515a54d8d74483662fb4c7
William-JosephSimons <billyjoe.ds05@gmail.com>
2024-09-28 16:13:13 +1000

    added testing for level filter

--- Milestone_2/test_all_functions.py
+++ Milestone_2/test_all_functions.py
@@ -9,7 +9,7 @@ def test_load_data_valid():
   # making a small dataframe for testing
   test_data = pd.DataFrame({
   'food': ["Peanut Butter", "Apple Pie", "Another Random Item", "Butter Scotch"],
-  'Poison': [345, 987654, 8765, 7],
+  'Poison': [345, 987, 531, 7],
   'Row Number': [1, 2, 3, 4]
   })
 
@@ -38,7 +38,7 @@ def test_search_function_valid():
   result2, row_num2 = af.search_function('', df)
   expected_result2 = pd.DataFrame({
   'food': ["Peanut Butter","Apple Pie", "Another Random Item", "Butter Scotch"],
-  'Poison': [345, 987654, 8765, 7],
+  'Poison': [345, 987, 531, 7],
   'Row Number': [1, 2, 3, 4]
   })
   pd.testing.assert_frame_equal(result2, expected_result2)
@@ -64,10 +64,10 @@ def test_num_of_row():
 
 def test_nutrition_range_filter_valid():
   # test for range with extreme values. Returns entire dataframe
-  result = af.nutrition_range_filter(df, 'Poison', minVal=-9999999, maxVal=9999999)
+  result = af.nutrition_range_filter(df, 'Poison', -9999999, 9999999)
   expected_result = pd.DataFrame({
   'food': ["Peanut Butter", "Apple Pie", "Another Random Item", "Butter Scotch"],
-  'Poison': [345, 987654, 8765, 7],
+  'Poison': [345, 987, 531, 7],
   'Row Number': [1, 2, 3, 4]
   })
   pd.testing.assert_frame_equal(result, expected_result)
@@ -77,19 +77,19 @@ def test_nutrition_range_filter_valid():
   pd.testing.assert_frame_equal(result2, expected_result)
 
   # test for valid range. Returns dataframe with values in range
-  result3 = af.nutrition_range_filter(df, 'Poison', minVal=300, maxVal=9000)
+  result3 = af.nutrition_range_filter(df, 'Poison', 300, 600)
   expected_result3 = pd.DataFrame({
   'food': ["Peanut Butter", "Another Random Item"],
-  'Poison': [345, 8765],
+  'Poison': [345, 531],
   'Row Number': [1, 3]
   }, index=[0, 2])
   pd.testing.assert_frame_equal(result3, expected_result3)
 
-  # test for also searching other columns
-  result4 = af.nutrition_range_filter(df, 'Row Number', minVal=2, maxVal=4)
+  # test for searching other columns
+  result4 = af.nutrition_range_filter(df, 'Row Number', 2, 4)
   expected_result4 = pd.DataFrame({
   'food': ["Apple Pie", "Another Random Item", "Butter Scotch"],
-  'Poison': [987654, 8765, 7],
+  'Poison': [987, 531, 7],
   'Row Number': [2, 3, 4]
   }, index=[1, 2, 3])
   pd.testing.assert_frame_equal(result4, expected_result4)
@@ -97,5 +97,43 @@ def test_nutrition_range_filter_valid():
 
 def test_nutrition_range_filter_invalid():
   # test for invalid column name. Fail returns 2
-  result5 = af.nutrition_range_filter(df, 'Does Not Exist', minVal=300, maxVal=9000)
+  result5 = af.nutrition_range_filter(df, 'Does Not Exist', 300, 600)
   assert result5 == 2
+
+def test_nutrition_level_filter_valid():
+  # test for low level
+  low = af.nutrition_level_filter(df, 'Poison', 'Low')
+  expected_low = pd.DataFrame({
+  'food': ["Butter Scotch"],
+  'Poison': [7],
+  'Row Number': [4]
+  }, index=[3])
+  pd.testing.assert_frame_equal(low, expected_low)
+
+  # test for mid level
+  mid = af.nutrition_level_filter(df, 'Poison', 'Mid')
+  expected_mid = pd.DataFrame({
+  'food': ["Peanut Butter", "Another Random Item"],
+  'Poison': [345, 531],
+  'Row Number': [1, 3]
+  }, index=[0, 2])
+  pd.testing.assert_frame_equal(mid, expected_mid)
+
+  # test for high level
+  high = af.nutrition_level_filter(df, 'Poison', 'High')
+  expected_high = pd.DataFrame({
+  'food': ["Apple Pie"],
+  'Poison': [987],
+  'Row Number': [2]
+  }, index=[1])
+  pd.testing.assert_frame_equal(high, expected_high)
+
+def test_nutrition_level_filter_invalid():
+  # test for no level input
+  assert af.nutrition_level_filter(df, 'Poison') == 3
+
+  # test for invalid column
+  assert af.nutrition_level_filter(df, 'Does Not Exist', 'Low') == 2
+
+  # test for invalid level
+  assert af.nutrition_level_filter(df, 'Poison', 'Extreme') == 3
