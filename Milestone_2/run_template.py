ef951cdf448c8c8768af959903266681ee82e022
braithicus <braith.lee@griffithuni.edu.au>
2024-10-03 17:56:52 +1000

    Bare minimum run_template.py so that the GUI can be opened.

--- Milestone_2/run_template.py
+++ Milestone_2/run_template.py
@@ -1,1 +1,23 @@
 #This is where the program will run from.
+
+import wx
+import template_frame
+
+
+class MyApp(wx.App):
+    def OnInit(self):
+        # Create an instance of the frame (your main window)
+        frame = template_frame.MyFrame1(None)
+
+        # Show the frame
+        frame.Show(True)
+
+        return True
+
+
+if __name__ == "__main__":
+    # Create a wx App instance
+    app = MyApp(False)
+
+    # Start the wx event loop
+    app.MainLoop()
